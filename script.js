0ef034f7abc128aa2ef2f003a87d157a1f5e75c7
Gustavo Muratalla <gusmuratalla@gmail.com>
2021-05-08 15:45:28 -0700

    added arrays and user input to be spliced and store in a array

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
 // Assignment code here
+var userInput = "";
+var arrySplit = "";
+
+//generator Functions
 function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);  //generates a lowercase letter.
 }
 
 function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);  //generates an uppercase letter.
 }
 
 function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);  //generates a random number.
 }
 
 function getRandomSymbol() {
-  const symbols = ' !"#$%&()*+,-./:;<=>?@[\]^_`{|}~';
-  return symbols[Math.floor(Math.random() * symbols.length)];
+  var symbols = "!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~";
+  return symbols[Math.floor(Math.random() * symbols.length)]; //generates random symbol from const symbols array.
 }
+//end generator functions
 
+
+
+//password generator function
 function generatePassword() {
-  console.log(getRandomLower());
-  console.log(getRandomUpper());
-  console.log(getRandomNumber());
-  console.log(getRandomSymbol());
-  return "check console log";
+
+  alert("The criteria for password generator are lowercase, uppercase, numeric, and/or special characters.");
+
+  userInput = prompt("Select criteria for password (you can select more than one) (use space): 1=lowercase 2=uppercase 3=numeric 4=special characters.\n");
+  userInput = userInput.split(" ");
+  arrySplit = userInput.slice(0, userInput.length); // this will store our user input paramiters and create and array for us to access later on.
+
+  return getRandomLower() + getRandomUpper() + getRandomNumber() + getRandomSymbol();
 }
 
 // Get references to the #generate element
